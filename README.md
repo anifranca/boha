fb8072d86e23d416c717f809f8d0fffa2de035b3
buccalon <buccalon@users.noreply.github.com>
2020-11-06 02:27:46 -0300

    Fetch pages from Zotero

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Online Library of Environmental History
+## Environmental History Online Library
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with
 [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
@@ -7,13 +7,13 @@ It is intended to generate a static site from the Zotero API.
 
 Endpoint: `https://api.zotero.org/groups/204820/items`
 
+- [Zotero Web API docs](https://www.zotero.org/support/dev/web_api/v3/basics)
+
 ## Getting Started
 
 First, run the development server:
 
 ```bash
-npm run dev
-# or
 yarn dev
 ```
 
@@ -22,25 +22,3 @@ result.
 
 You can start editing the page by modifying `pages/index.js`. The page
 auto-updates as you edit the file.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js
-  features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out
-[the Next.js GitHub repository](https://github.com/vercel/next.js/) - your
-feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the
-[Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme)
-from the creators of Next.js.
-
-Check out our
-[Next.js deployment documentation](https://nextjs.org/docs/deployment) for more
-details.
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch("https://api.zotero.org/groups/204820/items");
+  const items = await res.json();
+
+  return {
+    props: {
+      items,
+    },
+  };
+}
+
+export default function Home({ items }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>BOHA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -15,38 +27,18 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Biblioteca Online de História Ambiental{" "}
-          <code className={styles.code}>pages/index.js</code>
+          Biblioteca Online de História Ambiental
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {items.map((item) => (
+            <Link href={`/item/${encodeURIComponent(item.key)}`}>
+              <a className={styles.card}>
+                <h3>{item.data.title}</h3>
+                <p>{item.data.itemType}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
 
