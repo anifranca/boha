a4c6182598c75e49f2aa7a6df827c65e6d4aa564
Bruno Buccalon <buccalon@users.noreply.github.com>
2022-03-16 01:06:55 -0500

    Test without strict mode

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_flexsearch: true,
+  unstable_flexsearch: false,
   unstable_staticImage: true,
 })
 
-module.exports = withNextra(
-  {
-    reactStrictMode: true,
-    i18n: {
-      locales: ['en', 'pt', 'es'],
-      defaultLocale: 'en',
-    },
-  })
+module.exports = withNextra({
+  i18n: {
+    locales: ['en', 'pt', 'es'],
+    defaultLocale: 'en',
+  },
+})
